5e54a0947a8a46a8eaf03f4e568a6582c1303121
d4rk3r9923 <huytvo.2003@gmail.com>
2023-09-20 06:56:04 +0700

    SORT Tracking Algorithm (Kalman Filter)

--- sort.py
+++ sort.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import os
 import numpy as np
 import matplotlib
-
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
